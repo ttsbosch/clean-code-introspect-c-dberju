788dca5c180080d38dd4140d571a174ec003859f
dberju <berju1995@gmail.com>
2024-06-13 16:27:09 +0530

    Update headers in TradeRecord.hpp

--- TradeRecord.hpp
+++ TradeRecord.hpp
@@ -1,5 +1,11 @@
 #ifndef TRADERECORD_HPP
 #define TRADERECORD_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
 typedef struct {
     char SourceCurrency[256]; 
     char DestibationCurrency[256];
