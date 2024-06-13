40aa4f73ef0646e95781bb481b743436bf490e31
dberju <berju1995@gmail.com>
2024-06-13 16:13:41 +0530

    Add TradeRecord struct in  TradeRecord.hpp

--- TradeRecord.hpp
+++ TradeRecord.hpp
@@ -1,1 +1,10 @@
+#ifndef TRADERECORD_HPP
+#define TRADERECORD_HPP
+typedef struct {
+    char SourceCurrency[256]; 
+    char DestibationCurrency[256];
+    float Lots;
+    double Price;
+} TradeRecord;
+#endif
 
