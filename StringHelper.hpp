0ad5c908206323f9ca276a938d139954b9494b34
dberju <berju1995@gmail.com>
2024-06-13 16:28:30 +0530

    Update StringHelper.hpp

--- StringHelper.hpp
+++ StringHelper.hpp
@@ -6,4 +6,36 @@
 #include <errno.h>
 
 char** SplitString(const char* inputString, char delimiter);
+
+char** SplitString(const char* inputString, char delimiter) {
+    int delimiterCount = 0;
+    const char* ptr = inputString;
+    while (*ptr != '\0') {
+        if (*ptr++ == delimiter) {
+            delimiterCount++;
+        }
+    }
+
+    char** tokenSet = (char**)malloc(sizeof(char*) * (delimiterCount + 2));
+    int tokenSetIterator = 0;
+    ptr = inputString;
+    char* token = (char*)malloc(strlen(inputString) + 1);
+    int tokenIterator = 0;
+    while (*ptr != '\0') {
+        if (*ptr == delimiter) {
+            token[tokenIterator] = '\0';
+            tokenSet[tokenSetIterator++] = strdup(token);
+            tokenIterator = 0;
+        } else {
+            token[tokenIterator++] = *ptr;
+        }
+        ptr++;
+    }
+    token[tokenIterator] = '\0';
+    tokenSet[tokenSetIterator++] = strdup(token);
+    tokenSet[tokenSetIterator] = NULL;
+    free(token);
+    return tokenSet;
+}
+
 #endif
