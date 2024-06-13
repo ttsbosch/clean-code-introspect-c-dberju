84182599acbeb4b2a7e8fcf4e8285e164cec00d8
dberju <berju1995@gmail.com>
2024-06-13 16:29:07 +0530

    Update Converters.hpp

--- Converters.hpp
+++ Converters.hpp
@@ -4,4 +4,24 @@
 int tryToConvertStringtoInt(const char* InputString, int* resultValue);
 int tryToConvertStringtoDouble(const char* InputString, double* resultValue);
 
+
+
+int tryToConvertStringtoInt(const char* InputString, int* resultValue) {
+    char* endptr;
+    *resultValue = strtol(InputString, &endptr, 10);
+    if (endptr == InputString) {
+        return 0;
+    }
+    return 1;
+}
+
+int tryToConvertStringtoDouble(const char* InputString, double* resultValue) {
+    char* endptr;
+    *resultValue = strtod(InputString, &endptr);
+    if (endptr == InputString) {
+        return 0;
+    }
+    return 1;
+}
+
 #endif
