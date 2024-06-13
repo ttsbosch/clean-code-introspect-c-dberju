623631f35d53414e7a379d8868fa2fb26728ad0a
dberju <berju1995@gmail.com>
2024-06-13 16:19:45 +0530

    Update StringHelper.hpp

--- StringHelper.hpp
+++ StringHelper.hpp
@@ -1,6 +1,9 @@
+#ifndef STRINGHELPER_HPP
+#define STRINGHELPER_HPP
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 char** SplitString(const char* inputString, char delimiter);
+#endif
