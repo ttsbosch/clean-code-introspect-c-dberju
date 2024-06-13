6648cbfe7daf78f56942f847258fee51d33e9641
dberju <berju1995@gmail.com>
2024-06-13 16:33:25 +0530

    Update header file in Converters.hpp

--- Converters.hpp
+++ Converters.hpp
@@ -1,6 +1,9 @@
 #ifndef CONVERTERS_HPP
 #define CONVERTERS_HPP
 
+#include <stdio.h>
+#include <string.h>
+
 int tryToConvertStringtoInt(const char* InputString, int* resultValue);
 int tryToConvertStringtoDouble(const char* InputString, double* resultValue);
 
