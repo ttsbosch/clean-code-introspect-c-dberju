8dfd02c45920c598f0fbd0c4d4e7302a8c49daaf
dberju <berju1995@gmail.com>
2024-06-13 16:30:15 +0530

    Update CsvToXmlTradeRecordConverter.cpp

--- CsvToXmlTradeRecordConverter.cpp
+++ CsvToXmlTradeRecordConverter.cpp
@@ -3,55 +3,8 @@
 #include <string.h>
 #include <errno.h>
 
-char** SplitString(const char* inputString, char delimiter) {
-    int delimiterCount = 0;
-    const char* ptr = inputString;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiter) {
-            delimiterCount++;
-        }
-    }
-
-    char** tokenSet = (char**)malloc(sizeof(char*) * (delimiterCount + 2));
-    int tokenSetIterator = 0;
-    ptr = inputString;
-    char* token = (char*)malloc(strlen(inputString) + 1);
-    int tokenIterator = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[tokenIterator] = '\0';
-            tokenSet[tokenSetIterator++] = strdup(token);
-            tokenIterator = 0;
-        } else {
-            token[tokenIterator++] = *ptr;
-        }
-        ptr++;
-    }
-    token[tokenIterator] = '\0';
-    tokenSet[tokenSetIterator++] = strdup(token);
-    tokenSet[tokenSetIterator] = NULL;
-    free(token);
-    return tokenSet;
-}
-
-
-int tryToConvertStringtoInt(const char* InputString, int* resultValue) {
-    char* endptr;
-    *resultValue = strtol(InputString, &endptr, 10);
-    if (endptr == InputString) {
-        return 0;
-    }
-    return 1;
-}
-
-int tryToConvertStringtoDouble(const char* InputString, double* resultValue) {
-    char* endptr;
-    *resultValue = strtod(InputString, &endptr);
-    if (endptr == InputString) {
-        return 0;
-    }
-    return 1;
-}
+#include "Converters.hpp"
+#include "TradeRecord.hpp"
 
 void convertDataFromCsvToXml(FILE* stream) {
     char line[1024];
