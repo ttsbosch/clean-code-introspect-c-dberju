a259c2f8d125ecea8f29d376b46e04efa60d2d11
dberju <berju1995@gmail.com>
2024-06-13 16:22:30 +0530

    Update converter prototype in  Converters.hpp

--- Converters.hpp
+++ Converters.hpp
@@ -1,1 +1,7 @@
+#ifndef CONVERTERS_HPP
+#define CONVERTERS_HPP
 
+int tryToConvertStringtoInt(const char* InputString, int* resultValue);
+int tryToConvertStringtoDouble(const char* InputString, double* resultValue);
+
+#endif
